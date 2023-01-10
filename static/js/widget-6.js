e7e7308cc341424e13496d8d67257c4d453ea9e2
Joaquin Velazquez <velaz314@gmail.com>
2023-01-10 12:16:46 -0300

    Corrigiendo colores de texto en publicaciones v4

--- static/js/widget-6.js
+++ static/js/widget-6.js
@@ -2,6 +2,9 @@ const URLactual = window.location.pathname;
 const texto = document.querySelectorAll('.ui-pdp-container__row--main-actions .andes-button__content');
 
 if (URLactual !== "/") {
-    texto['0'].style.filter = 'invert(1)';
-    texto['1'].style.filter = 'invert(64%) sepia(84%) saturate(2361%) hue-rotate(357deg) brightness(98%) contrast(98%)';
+    $(document).ready(function () {
+        texto['0'].style.filter = 'invert(1)';
+        texto['1'].style.filter = 'invert(64%) sepia(84%) saturate(2361%) hue-rotate(357deg) brightness(98%) contrast(98%)';
+      });
 }
+
